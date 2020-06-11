355b5b86e3cc3fb448a5ebfd4f41ead7e012bd1b
luis gullermo <luisguillermotrujillo16@gmail.com>
2020-06-10 20:35:02 -0500

    cambios luis

--- script/script.js
+++ script/script.js
@@ -6,3 +6,12 @@ function openNav() {
 function closeNav() {
     document.getElementById("menu").style.height = "0%";
 }
+function quitarBotom(mediaQ) {
+    if (mediaQ.matches) {
+        let borrarBoton = document.getElementById('borrarBoton')
+        borrarBoton.remove()
+    }
+}
+let mediaQ = window.matchMedia('(min-width:1000px)')
+quitarBotom(mediaQ)
+mediaQ.addEventListener('quitarBotom')
